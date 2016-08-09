deb5b63ea34f42a09522098b95f5424ebf485a9d
Schmalte <Schmalte@xyz.com>
2016-08-09 17:43:20 +0200

    FEATURE: Adding couple of plugins and Features of gulp to the index.html

--- gulp-test-project/project1/gulpfile.js
+++ gulp-test-project/project1/gulpfile.js
@@ -1,15 +1,32 @@
-//Initializing the gulp object.
+//Initializing gulp.
 var gulp = require('gulp');
 
 //Loading plugins
 var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
 
-gulp.task('default',function(){
+//* Wildcard for checking all .js files in the folder.
+var SRC = 'js/*.js';
+var DEST = 'dist';
 
+gulp.task('changed', function(){
+  return gulp.src(SRC)
+        .pipe(plumber())
+        .pipe(changed(DEST))
+        .pipe(gulp.dest(DEST));
 });
+
+gulp.task('default',['jshint']);
 
 gulp.task('jshint', function(){
   gulp.src('js/main.js')
+  .pipe(plumber())
   .pipe(jshint())
   .pipe(jshint.reporter('default'));
+});
+
+gulp.task('monitor', function(){
+  console.log('found a change in a .js file: process file....')
+  gulp.watch(SRC,['changed']);
 });
